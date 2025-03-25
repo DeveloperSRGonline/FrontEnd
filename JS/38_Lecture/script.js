b59e61f794c3f24c016174135456c99fa5a9a639
Shivam Garade <shivamgarade05@gmail.com>
2025-03-25 22:19:19 +0530

    class 38

---
+++ JS/38_Lecture/script.js
@@ -0,0 +1,19 @@
+/*
+Math.random() - inbuilt method 
+-- range form => 0 to 1 
+-- 1 & 0 nahi aata  bichi ki value aati hai 
+
+var num = Math.floor(Math.random()*100)
+console.log(num);
+
+template literals 
+var a = 19;
+console.log(`hello ${a} bye`);
+
+Project - 1: Random color generator
+Project - 2: Picking random values form array 
+
+*/
+
+var num = Math.floor(Math.random()*100)
+console.log(num);
