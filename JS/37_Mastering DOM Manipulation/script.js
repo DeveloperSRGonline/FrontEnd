a7bf54ec50885e5245dfb46b4799759922336114
Shivam Garade <shivamgarade05@gmail.com>
2025-03-25 20:19:06 +0530

    DOM manipulation

---
+++ JS/37_Mastering DOM Manipulation/script.js
@@ -0,0 +1,27 @@
+/*
+javascript :
+    DOM
+    Backend
+    libraries
+    dextop apps
+    mobile apps
+
+DOM - Document Object Model 
+- frontend ki javascript 
+
+4 Pillors of javascript:
+    - selection of elements ✅
+    - changing html ✅
+    - changing css
+    - event listener 
+     
+    
+    
+    
+    
+    
+    */
+var h1 = document.querySelector('h1')
+
+h1.style.color = 'yellow'
+h1.style.backgroundColor = '#ff0000'
