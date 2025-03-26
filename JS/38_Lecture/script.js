549bd41fc645195d838cea0e33b4f1c224047013
Shivam Garade <shivamgarade05@gmail.com>
2025-03-26 12:29:00 +0530

    class 38

--- JS/38_Lecture/script.js
+++ JS/38_Lecture/script.js
@@ -17,3 +17,46 @@ Project - 2: Picking random values form array
 
 var num = Math.floor(Math.random()*100)
 console.log(num);
+
+var arr = [
+    {
+        team:'CSK',
+        primary:'yellow',
+        secondary:'green'
+    },
+    {
+        team:'RCB',
+        primary:'red',
+        secondary:'black'
+    },
+    {
+        team:'MI',
+        primary:'blue',
+        secondary:'gold'
+    },
+    {
+        team:'KKR',
+        primary:'purple',
+        secondary:'gold'
+    },
+    {
+        team:'SRH',
+        primary:'orange',
+        secondary:'black'
+    }
+]
+
+var btn = document.querySelector('button')
+var h1 = document.querySelector('h1')
+
+btn.addEventListener('click',function(){
+    var num = Math.floor(Math.random()*arr.length)
+
+    var winner = arr[num]
+
+    h1.innerHTML = winner.team
+    h1.style.backgroundColor = winner.primary
+    h1.style.color = winner.secondary
+})
+
+// https://github.com/saarthack/live1
